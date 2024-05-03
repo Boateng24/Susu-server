1777301970e6c4e26d49b5f54481070482a3fd2e
Tuffour Boateng <tuffour.boateng@amalitech.com>
2024-05-03 09:37:20 +0200

    api docs updated

--- src/helpers/setupSwagger.ts
+++ src/helpers/setupSwagger.ts
@@ -2,13 +2,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app) => {
   const config = new DocumentBuilder()
-    .setTitle('School Sphere API Documentation')
+    .setTitle('Susu Server API documentation')
     .setDescription(
-      'This is the official API documentation of the schoolsphere app',
+      'This is the official API documentation of the susu server app',
     )
     .setVersion('1.0')
     .addBearerAuth()
-    .addTag('SCHOOLSPHERE')
+    .addTag('SUSU SERVER')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
